5a438a7e321a108789e3f85a32f4019ef05854ab
quanbinn <quanbinn@126.com>
2018-06-21 09:53:38 +0800

    revise

--- code/Done/章1-快速掌握编程的基础知识/while_loop_neckpain.py
+++ code/Done/章1-快速掌握编程的基础知识/while_loop_neckpain.py
@@ -1,17 +1,17 @@
 neckPainRecord = []
-neckPainRecord.append("satisfactory")
-neckPainRecord.append("a little satisfactory")
-neckPainRecord.append("a little painful")
-neckPainRecord.append("a little painful")
-neckPainRecord.append("painful")
+neckPainRecord.append("No pain")
+neckPainRecord.append("Mild Pain")
+neckPainRecord.append("Mild Pain")
+neckPainRecord.append("Moderate Pain")
+neckPainRecord.append("Mild Pain")
 
 i = 0
 
-while neckPainRecord[i] != "painful":
-    print("Day",i,"的颈部肌肉疼痛程度是：",neckPainRecord[i])
+while neckPainRecord[i] != "Moderate Pain":
+    print("Day",i,"：我的颈部肌肉疼痛程度是：",neckPainRecord[i])
     i = i + 1
     
-print("Day",i,"的颈部肌肉疼痛程度是：",neckPainRecord[i])
+print("Day",i,"：我的颈部肌肉疼痛程度是：",neckPainRecord[i])
 
 # pythontutor's permanent link:
-# http://pythontutor.com/visualize.html#code=neckPainRecord%20%3D%20%5B%5D%0AneckPainRecord.append%28%22satisfactory%22%29%0AneckPainRecord.append%28%22a%20little%20satisfactory%22%29%0AneckPainRecord.append%28%22a%20little%20painful%22%29%0AneckPainRecord.append%28%22a%20little%20painful%22%29%0AneckPainRecord.append%28%22painful%22%29%0A%0Ai%20%3D%200%0A%0Awhile%20neckPainRecord%5Bi%5D%20!%3D%20%22painful%22%3A%0A%20%20%20%20print%28%22Day%22,i,%22%E7%9A%84%E9%A2%88%E9%83%A8%E8%82%8C%E8%82%89%E7%96%BC%E7%97%9B%E7%A8%8B%E5%BA%A6%E6%98%AF%EF%BC%9A%22,neckPainRecord%5Bi%5D%29%0A%20%20%20%20i%20%3D%20i%20%2B%201%0A%20%20%20%20%0Aprint%28%22Day%22,i,%22%E7%9A%84%E9%A2%88%E9%83%A8%E8%82%8C%E8%82%89%E7%96%BC%E7%97%9B%E7%A8%8B%E5%BA%A6%E6%98%AF%EF%BC%9A%22,neckPainRecord%5Bi%5D%29&cumulative=false&heapPrimitives=nevernest&mode=edit&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false
+# http://pythontutor.com/visualize.html#code=neckPainRecord%20%3D%20%5B%5D%0AneckPainRecord.append%28%22No%20pain%22%29%0AneckPainRecord.append%28%22Mild%20Pain%22%29%0AneckPainRecord.append%28%22Mild%20Pain%22%29%0AneckPainRecord.append%28%22Moderate%20Pain%22%29%0AneckPainRecord.append%28%22Mild%20Pain%22%29%0A%0Ai%20%3D%200%0A%0Awhile%20neckPainRecord%5Bi%5D%20!%3D%20%22Moderate%20Pain%22%3A%0A%20%20%20%20print%28%22Day%22,i,%22%EF%BC%9A%E6%88%91%E7%9A%84%E9%A2%88%E9%83%A8%E8%82%8C%E8%82%89%E7%96%BC%E7%97%9B%E7%A8%8B%E5%BA%A6%E6%98%AF%EF%BC%9A%22,neckPainRecord%5Bi%5D%29%0A%20%20%20%20i%20%3D%20i%20%2B%201%0A%20%20%20%20%0Aprint%28%22Day%22,i,%22%EF%BC%9A%E6%88%91%E7%9A%84%E9%A2%88%E9%83%A8%E8%82%8C%E8%82%89%E7%96%BC%E7%97%9B%E7%A8%8B%E5%BA%A6%E6%98%AF%EF%BC%9A%22,neckPainRecord%5Bi%5D%29&cumulative=false&heapPrimitives=nevernest&mode=edit&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false
